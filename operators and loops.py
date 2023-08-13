e458768fda756a466118d14fe602018122de3e16
kusu9652 <116740186+kusu9652@users.noreply.github.com>
2023-08-13 20:54:39 +0530

    Add files via upload

---
+++ operators and loops.py
@@ -0,0 +1,65 @@
+#write a function for arithmetic operators(+,-,*,/)
+def function():
+    x=8
+    y=6
+    z=x+y
+    h=x-y
+    l=x*y
+    b=x/y
+    print(" ",z)
+    print(" ",h)
+    print(" ",l)
+    print(" ",b)
+function()
+#write a program find two numbers equal or not
+z=8
+k=9
+if z==k:
+    print("equal")
+else:
+    print(" not equal")
+#program for releational operators(<,<=,>,>=)  
+z=9
+k=9
+if z<=k:
+    print("hm")
+else:
+    print("ho")
+    
+
+z=8
+k=8
+if z>=k:
+    print("hell")
+else:
+    print("hai")
+    
+
+z=10
+k=9
+if z<k:
+    print("sky")
+else:
+    print("moon")
+    
+    
+z=8
+k=9
+if z>k:
+    print("sun")
+else:
+    print("heavy")
+#print smaller and larger numbers    
+s=8
+k=9
+if s < k:
+    print("smaller")
+    
+d=10
+l=2
+if d > l:
+    print("larger")
+
+
+
+
