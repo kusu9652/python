2996bc1f94f94ff53e15b250f364376b83355637
kusu9652 <116740186+kusu9652@users.noreply.github.com>
2023-08-13 20:53:16 +0530

    Add files via upload

---
+++ functions and calling.py
@@ -0,0 +1,30 @@
+#write two methods with same name and same number of parameters of same type
+def kusu(a,b):
+    c=a+b
+    print(c)
+def kusu(a,b):
+    c=a+b
+    print(c)
+kusu(12,15)
+kusu(12,15)
+#write two methods of same name but different number of parameters of different data types to call
+
+def kusu(a,b):
+    c=a+b
+    print(c)
+def kusu(a,b,c):
+    k=a+b+c
+    print(k)
+
+kusu(5,0.6,1.3)
+kusu(8,0.2,3.6)
+#write two methods of same name but different number of parameters of same types to call
+def kusu(a,b):
+    c=a+b
+    print(c)
+def kusu(a,b,c):
+    k=a+b+c
+    print(k)
+
+kusu(5,2,3)
+kusu(8,4,3)
