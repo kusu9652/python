64da9d5278dff8c9d314906b1a0d7011c4b2b3c9
kusu9652 <116740186+kusu9652@users.noreply.github.com>
2023-08-13 20:56:24 +0530

    Add files via upload

---
+++ printing statement & data types.py
@@ -0,0 +1,33 @@
+#write a program to print your name
+print("kusuma")
+#single line & multiline comments
+print('she')
+print("hey")
+print("""he""")
+a=5
+#
+a=5
+print("type of a:",type(a))
+b=5.0
+print("type of b:",type(b))
+c=True
+print("type of c:",type(c))
+d="97"
+print("type of d:",type(d))
+e="123748495L"
+print("type of e:",type(e))
+#local and global scope
+def oh():
+    print("yeah")
+def func():
+    s="aha"
+    print(" ",s)
+k="ohh"
+func()
+print(" ",k)
+oh()
+
+
+
+
+
